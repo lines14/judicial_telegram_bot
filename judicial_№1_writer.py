d76d068760cc10c627d73d0dad1e1a2c6e73926d
Anton Lipatov <tonybass1414@yahoo.com>
2022-12-31 04:31:40 +0600

    feat:add functions to make table cells right-align

--- judicial_№1_writer.py
+++ judicial_№1_writer.py
@@ -1,6 +1,7 @@
 # petition for familiarization with the case materials:
 
 import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 import os
 import dotenv
 
@@ -39,12 +40,21 @@ VAR30 = os.environ.get('VAR30')
 VAR31 = os.environ.get('VAR31')
 VAR32 = os.environ.get('VAR32')
 
-def make_rows_bold(*rows):
+def make_table_rows_bold(*rows):
     for row in rows:
         for cell in row.cells:
             for paragraph in cell.paragraphs:
                 for run in paragraph.runs:
                     run.font.bold = True
+
+#     font = paragraph.runs[0].font
+#     font.size= Pt(10)
+
+def make_table_columns_align_right(*columns):
+    for column in columns:
+        for cell in column.cells:
+            for paragraph in cell.paragraphs:
+                paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
 document = docx.Document()
 
@@ -69,17 +79,14 @@ for first_col, second_col in records:
     row_cells = table.add_row().cells
     row_cells[0].text = first_col
     row_cells[1].text = second_col
-
-make_rows_bold(table.rows[0], table.rows[2], table.rows[7])
+make_table_rows_bold(table.rows[0], table.rows[2], table.rows[7])
+make_table_columns_align_right(table.columns[0])
 
 document.add_paragraph(VAR23)
-
 para = document.add_paragraph('')
 para.add_run(VAR24).bold = True
-
 para = document.add_paragraph('')
 para.add_run(VAR25).bold = True
-
 document.add_paragraph(VAR26)
 document.add_paragraph(VAR27)
 document.add_paragraph(VAR28)
@@ -90,9 +97,7 @@ table = document.add_table(rows=1, cols=2)
 hdr_cells = table.rows[0].cells
 hdr_cells[0].text = VAR31
 hdr_cells[1].text = VAR32
-
-make_rows_bold(table.rows[0])
-
-document.add_paragraph('')
+make_table_rows_bold(table.rows[0])
+make_table_columns_align_right(table.columns[1])
 
 document.save('/home/lines14/projects/judicial_telegram_bot/judicial_№1_writer.docx')
