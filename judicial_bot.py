cff0d307fcdb41d300d91f1b941556da31793e0a
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-13 14:45:24 +0000

    feat: finalize the webhook

--- judicial_bot.py
+++ judicial_bot.py
@@ -1,7 +1,6 @@
 import logging
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils.executor import start_webhook
-from aiogram.types import InputFile
 from modules.config import WEBHOOK_PATH, WEBHOOK_URL, WEBAPP_HOST, WEBAPP_PORT
 from modules.bot_base import bot
 from modules.bot_base import dp
@@ -9,13 +8,11 @@ from modules.bot_base import dp
 logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
 
-# CERT=InputFile('/home/lines14/projects/judicial_telegram_bot/judicial_bot_public.pem', 'r')
-
-async def on_startup(dp):
-    await bot.set_webhook(url=WEBHOOK_URL) #drop_pending_updates=True
+async def on_startup(_):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True) #certificate=CERT
     print('Бот успешно запущен!')
 
-async def on_shutdown(dp):
+async def on_shutdown(_):
     logging.warning('Shutting down..')
     await bot.delete_webhook()
     await dp.storage.close()
@@ -27,4 +24,4 @@ from modules import handlers
 handlers.register_handler_client(dp)
 
 if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown, host=WEBAPP_HOST, port=WEBAPP_PORT, )
+    start_webhook(dp, WEBHOOK_PATH, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown, host=WEBAPP_HOST, port=WEBAPP_PORT, )
