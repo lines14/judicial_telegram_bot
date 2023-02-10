aa01b7c93dbc3154e7bca36abeb171000ce1101d
Anton Lipatov <tonybass1414@yahoo.com>
2023-02-10 22:49:18 +0600

    add some callback query answers

--- modules/admin_handlers.py
+++ modules/admin_handlers.py
@@ -343,9 +343,11 @@ async def stage_changer(callback: types.CallbackQuery, state: FSMContext):
             callback_slicer_2 = ' '.join(callback.data.split(' ')[slice(1,3)])
             if callback_slicer_1 == 'inwork':
                 await data_base.sql_stage_changer(callback_slicer_2, '🟡В работе')
+                await bot.answer_callback_query(callback.id)
                 # await bot.send_message(chat_id = callback.from_user.id, text=f'В работе {callback_slicer_2}')
             else:
                 await data_base.sql_stage_changer(callback_slicer_2, '🔴Завершено')
+                await bot.answer_callback_query(callback.id)
                 # await bot.send_message(chat_id = callback.from_user.id, text=f'Завершили {callback_slicer_2}')
 
 # Регистратура хэндлеров бота
